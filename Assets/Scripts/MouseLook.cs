2b34f329018aa7bd548575b67a1107a3a3544a2c
Cyborger <joe.zlonicky@gmail.com>
2019-04-20 19:00:35 -0700

    Initial commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    private Vector2 _mouseLook;
+    private Vector2 _smoothV;
+
+    public float sensitivity;
+    public float smoothing;
+
+    private GameObject _character;
+    
+    // Start is called before the first frame update
+    private void Start() {
+        _character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        var moveDirection = new Vector2(Input.GetAxisRaw("Mouse X"),
+            Input.GetAxisRaw("Mouse Y"));
+        moveDirection = Vector2.Scale(moveDirection, new Vector2(sensitivity * smoothing,
+            sensitivity * smoothing));
+        _smoothV.x = Mathf.Lerp(_smoothV.x, moveDirection.x, 1f / smoothing);
+        _smoothV.y = Mathf.Lerp(_smoothV.y, moveDirection.y, 1f / smoothing);
+        _mouseLook += _smoothV;
+        
+        transform.localRotation = Quaternion.AngleAxis(-_mouseLook.y, Vector3.right);
+        _character.transform.localRotation = Quaternion.AngleAxis(_mouseLook.x, _character.transform.up);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float strafeSpeed;
+
+    private bool _isLocked;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        _isLocked = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        movement *= speed * Time.deltaTime;
+        transform.Translate(movement);
+
+        if (_isLocked && Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            _isLocked = false;
+        } else if (!_isLocked && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            _isLocked = true;
+        }
+    }
+}
