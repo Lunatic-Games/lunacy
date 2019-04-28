46f77d6574e3507d913aaf81cd65aeba9cfb6af2
Cyborger <joe.zlonicky@gmail.com>
2019-04-28 11:40:58 -0700

    Added jumping - need to fix camera jitter when falling
    Raycasting for jumping is credited to matthew goodall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
 
     private bool _isLocked;
+    private bool _isGrounded = true;
+    private Rigidbody _rb;
+    private Vector3 _jumpVector;
+    private RaycastHit _groundRayCast;
     
     // Start is called before the first frame update
-    private void Start()
-    {
+    private void Start() {
+        _rb = GetComponent<Rigidbody>();
+        _jumpVector = new Vector3(0.0f, jumpForce, 0.0f);
         Cursor.lockState = CursorLockMode.Locked;
         _isLocked = true;
     }
 
+    private void OnCollisionEnter(Collision other) {
+        if (_isGrounded == false && Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out _groundRayCast,
+                Mathf.Infinity)) 
+        {
+            
+            if (_groundRayCast.transform.tag == "Ground" && _groundRayCast.distance <= 2.6){
+                _isGrounded = true;
+            }
+        }
+    }
+
     // Update is called once per frame
-    private void Update()
-    {
+    private void Update() {
         var movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
         movement *= speed * Time.deltaTime;
         transform.Translate(movement);
 
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
+            _rb.AddForce(_jumpVector, ForceMode.Impulse);
+            _isGrounded = false;
+        }
         if (_isLocked && Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -31,5 +51,7 @@ public class PlayerController : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
             _isLocked = true;
         }
+
+        
     }
 }
